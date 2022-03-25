51b787cb4044e78cc59037bd8f2494db52ee4a30
zhaoxianbing <37646484+xiaobaicaihhh@users.noreply.github.com>
2022-03-25 14:29:44 +0800

    Update global_config.py

--- global_config.py
+++ global_config.py
@@ -1,11 +1,3 @@
-'''
-Author: your name
-Date: 2022-03-25 09:41:43
-LastEditTime: 2022-03-25 10:31:42
-LastEditors: your name
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /SPAMN/global_config.py
-'''
 import os
 import torch
 
