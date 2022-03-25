b0a200ec7b0a88c108264b871123ac37c7bcf264
zhaoxianbing <37646484+xiaobaicaihhh@users.noreply.github.com>
2022-03-25 15:44:37 +0800

    Update download.sh

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,11 +1,3 @@
-###
- # @Author: your name
- # @Date: 2022-03-25 09:31:44
- # @LastEditTime: 2022-03-25 09:31:45
- # @LastEditors: Please set LastEditors
- # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- # @FilePath: /SPAMN/datasets/download.sh
-### 
 #!/bin/bash
 #if the follwings command do not work then just simply copy paste the link in browser
 wget https://www.dropbox.com/s/sv94igp7zi3rsj1/mosi.pkl?dl=1
