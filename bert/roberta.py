b1792a8e2ee21c3808255f03abcbccfdbc3e3beb
zhaoxianbing <37646484+xiaobaicaihhh@users.noreply.github.com>
2022-03-25 15:45:21 +0800

    Update roberta.py

--- bert/roberta.py
+++ bert/roberta.py
@@ -1,11 +1,3 @@
-'''
-Author: your name
-Date: 2022-01-19 20:04:51
-LastEditTime: 2022-03-25 09:36:28
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /BERT_multimodal_transformer/roberta.py
-'''
 import torch
 import torch.nn as nn
 
